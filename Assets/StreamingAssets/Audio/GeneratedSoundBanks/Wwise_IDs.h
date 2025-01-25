98c5a9e52afb2446de3b4e92b4e4c39105d9f7be
peaveybandit257 <157910748+peaveybandit257@users.noreply.github.com>
2025-01-25 14:53:28 -0600

    Soundbanks

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,73 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID MUS_CLIMAX = 1220128597U;
+        static const AkUniqueID MUS_ESCAPE = 2215620476U;
+        static const AkUniqueID MUS_INTRO = 1219230789U;
+        static const AkUniqueID MUS_MENU = 3149643052U;
+        static const AkUniqueID MUS_TENSION = 1172924437U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace GAMEPLAY_AREA
+        {
+            static const AkUniqueID GROUP = 2638858007U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CLIMAX = 1377027961U;
+                static const AkUniqueID ESCAPE = 3169107872U;
+                static const AkUniqueID INTRO = 1125500713U;
+                static const AkUniqueID TENSION = 1571361561U;
+            } // namespace SWITCH
+        } // namespace GAMEPLAY_AREA
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID BUBBLE_SOUNDBANK = 2113579301U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
