5f329d66d66073ab5b082e214086da62a4d0f439
peaveybandit257 <157910748+peaveybandit257@users.noreply.github.com>
2025-01-26 09:15:29 -0600

    Most of audio

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,28 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID DIA_CLIMAXSTORY1 = 3033011476U;
+        static const AkUniqueID DIA_ESCAPESTORY1 = 3260869773U;
+        static const AkUniqueID DIA_ESCAPESTORY2 = 3260869774U;
+        static const AkUniqueID DIA_ESCAPESTORY3 = 3260869775U;
+        static const AkUniqueID DIA_INTROSTORY1 = 2922889338U;
+        static const AkUniqueID DIA_INTROSTORY2 = 2922889337U;
+        static const AkUniqueID DIA_INTROSTORY3 = 2922889336U;
+        static const AkUniqueID DIA_INTROSTORY4 = 2922889343U;
+        static const AkUniqueID DIA_JELLYWARNING = 1029159304U;
+        static const AkUniqueID DIA_SHARKWARNING = 2181865273U;
+        static const AkUniqueID DIA_TENSIONSTORY1 = 3417468250U;
+        static const AkUniqueID DIA_TENSIONSTORY2 = 3417468249U;
+        static const AkUniqueID DIA_TENSIONSTORY3 = 3417468248U;
+        static const AkUniqueID DIA_TENSIONSTORY4 = 3417468255U;
+        static const AkUniqueID DIA_TREASUREBONE = 3581270229U;
+        static const AkUniqueID DIA_TREASURECUTLASS = 2993629078U;
+        static const AkUniqueID DIA_TREASUREDOUBLOON = 3494670805U;
+        static const AkUniqueID DIA_TREASUREFLINTLOCK = 1517707933U;
+        static const AkUniqueID DIA_TREASUREGEMSTONE = 1661916387U;
+        static const AkUniqueID DIA_TREASUREHAT = 624438454U;
+        static const AkUniqueID DIA_TREASURESHIPWRECK = 511787789U;
+        static const AkUniqueID DIA_VICTORY = 779849494U;
         static const AkUniqueID MUS_CLIMAX = 1220128597U;
         static const AkUniqueID MUS_ESCAPE = 2215620476U;
         static const AkUniqueID MUS_GAMEPLAY = 895625293U;
@@ -56,6 +78,7 @@ namespace AK
                 static const AkUniqueID CLIMAX = 1377027961U;
                 static const AkUniqueID ESCAPE = 3169107872U;
                 static const AkUniqueID INTRO = 1125500713U;
+                static const AkUniqueID MENU = 2607556080U;
                 static const AkUniqueID TENSION = 1571361561U;
             } // namespace SWITCH
         } // namespace GAMEPLAY_AREA
@@ -85,7 +108,10 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID DIALOGUE_BUS = 3714968413U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC_BUS = 3127962312U;
+        static const AkUniqueID SFX_BUS = 1502772432U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
